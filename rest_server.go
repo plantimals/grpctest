d54f83a2298f795cfc9cade02f4d919ab6d41a08
plantimals <sartorius.snouth@gmail.com>
2017-01-28 22:15:55 -0600

    implemented stubb rest server

---
+++ rest_server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	gw "github.com/plantimals/grpctest/coffee"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	echoEndpoint = flag.String("echo_endpoint", "localhost:50051", "endpoint of YourService")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterCoffeeServiceHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	http.ListenAndServe(":8080", mux)
+	log.Fatal("died")
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
